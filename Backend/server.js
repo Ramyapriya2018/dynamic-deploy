762c8c814b073e9e12864e96ea0efbf2d6844bd7
Ramya <shanmugapriya183@gmail.com>
2026-03-13 13:10:58 +0530

    backend ready for deployment port

--- Backend/server.js
+++ Backend/server.js
@@ -48,7 +48,8 @@ app.delete("/users/:id", (req, res) => {
     res.json({ message: "User deleted" });
   });
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Backend running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
